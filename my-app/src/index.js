f7fff336d700a94b65dda2011097cb15a93db55f
ewa1siwek <ew.siwek@gmail.com>
2018-08-02 16:19:08 +0200

    Added handleClick to the Board class

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,11 +23,17 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
         return (
         <Square
             value={this.state.squares[i]}
-            onclick={() => this.handleClick(i)}
+            onClick={() => this.handleClick(i)}
         />
         );
     }
