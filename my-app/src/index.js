971efbc4860221b84aafdce284b3c92e9c5a99d5
ewa1siwek <ew.siwek@gmail.com>
2018-08-02 16:10:19 +0200

    Passing down props value and onClick from Board to Square

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,21 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({value: 'X'})}
+                onClick={() => this.props.onClick()}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
-    );
+        );
     }
 }
 
@@ -28,8 +22,14 @@ class Board extends React.Component {
             squares: Array(9).fill(null),
         };
     }
+
     renderSquare(i) {
-        return <Square value={i}/>;
+        return (
+        <Square
+            value={this.state.squares[i]}
+            onclick={() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
